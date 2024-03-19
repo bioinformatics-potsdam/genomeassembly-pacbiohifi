da718dbaf4e07988ae9447bcd690eeb8e87ad295
Gaurav Sablok <sablokg@gmail.com>
2024-03-19 10:49:27 +0100

    Create extractingdiagonalinformation.py

---
+++ extractingdiagonalinformation.py
@@ -0,0 +1,4 @@
+#Universitat Potsdam
+#Author Gaurav Sablok
+#Date 2024-3-19
+# a file operation method for extracting the diagonal information from the reads alignments and then using the diagonals as the nodes clustering in the graphs.
