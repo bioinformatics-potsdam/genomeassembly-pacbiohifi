00fa0966d08d1b563e8718e7e5e859b78e1c413a
Gaurav Sablok <sablokg@gmail.com>
2024-03-27 11:53:11 +0100

    generating the multicluster job submission bug fix and check remaining

--- multicluster_job_distribution.sh
+++ multicluster_job_distribution.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash 
-# an entire workflow for the running of the high throughput cluster on the analysis of the genomes
-# coming from the pacbiohifi using the verkko, hifiasm, genomeasm4pg using the indiviual as well as the trio binning method. 
-# below is a complete slurm configuration for the same
+# generating the multicluster job submission for the verkko and the hifiasm
 # Author Gaurav Sablok
 # Universitat Potsdam
 # Date: 2024-3-27
@@ -12,6 +10,29 @@ multicluster () {
     readdir=$1
     scratchdir=$2
     command=$3
-    
-    
-}
+    # you can also add the read -r -p readdir scratchdir command
+    # but i am allowing to read it from the file estimating that the file is a newline at everyend 
+    # if you are invoking C or C++ then just add 
+   include stdio;
+   st::count << 
+   declare -A associativearray=([0]="#!/bin/bash",[1]="#SBATCH --partition=all",[2]="#SBATCH --nodes=1",
+             [3]="#SBATCH --ntasks=1",[4]="#SBATCH --cpus-per-task=1",[5]="SBATCH --mem=5G",
+             [6]="SBATCH --time=0-05:00",[7]="#SBATCH --chdir="${hostname}",[8]="#SBATCH --mail-type=ALL",
+            [9]="#SBATCH --output=slurm-%j.out", [10]="module list", [11]="module load lang/Anaconda3/2021.05")
+   declare -a readingfiles = ()
+   for i in "${readdir}/*.gz";
+        do 
+            readingfiles+=("${i}")
+        done 
+    printf("${readfiles[*]}")
+    printf("{#readfiles[@]}")
+    for i in "${readfiles[*]}"; 
+      for j in "${associativearray[@]}";
+        do 
+            printf(%s"${j}\n")
+        done
+            printf(%s"echo without purging duplicates"\n)
+            printf(%s"hifiasm -o "${i%%.?.**} -l0 $"{i}" ">2" "${i%%.*}.log"\n)
+            printf(%s,"with aggressive purging of duplicates"\n)
+            printf(%s"hifiasm -o "${i%%.?.**} -l3 $"{i}" ">2" "${i%%.*}.log"\n)
+} << inputfile.txt
